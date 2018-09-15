684a42fef98f77c083f1bf0dccce92bca166af5d
cuterinrin <43025706+cuterinrin@users.noreply.github.com>
2018-09-16 01:03:30 +0900

    Update 전프기0913.c

--- 전프기0913.c
+++ 전프기0913.c
@@ -4,8 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Hello, Sookmyung Electronics!\n jjj");
-	printf("222");
+	printf("Hello, Sookmyung Electronics!");
 	return 0;
 }
  
